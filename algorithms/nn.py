fe5ee4df60cf4282f6f6063a946af1af38655d07
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2018-10-19 18:04:48 +0200

    Implement kNN.

--- algorithms/nn.py
+++ algorithms/nn.py
@@ -1,2 +1,12 @@
-# Nearest Neighbour Classifier
+from sklearn.neighbors.classification import KNeighborsClassifier
 
+
+def get_classifier():
+    return KNeighborsClassifier()
+
+
+def get_params_space(data_set):
+    return {
+        'n_neighbors': [1, 2, 3, 4, 5],
+        'metric': ['euclidean', 'manhattan']
+    }
