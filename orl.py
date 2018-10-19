935d024b8bea7169b2aee0eae22eb7c78dc6746e
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2018-10-19 14:24:57 +0200

    Ensure train-test split is 30% and stratified.

--- orl.py
+++ orl.py
@@ -7,10 +7,10 @@ def image_shape():
     return 30, 40
 
 
-def load_data(test_size=0.16, random_state=42):
+def load_data(test_size=0.3, random_state=42):
     data_dir = os.path.join(os.path.dirname(__file__), 'data')
     orl_data_path = os.path.join(data_dir, 'orl_data.txt')
     orl_labels_path = os.path.join(data_dir, 'orl_lbls.txt')
     X = np.loadtxt(orl_data_path).transpose()
     y = np.loadtxt(orl_labels_path, dtype=np.uint8)
-    return train_test_split(X, y, test_size=test_size, random_state=random_state)
+    return train_test_split(X, y, stratify=y, test_size=test_size, random_state=random_state)
