2bf7dd563df1cb9bdb457f6ee99871e5207a79d2
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2018-10-19 15:11:58 +0200

    Module with Nearest Centroid Classifier.

--- algorithms/ncc.py
+++ algorithms/ncc.py
@@ -1,1 +1,12 @@
-# Nearest Class Centroid Classifier
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+
+
+def get_classifier():
+    return NearestCentroid()
+
+
+def get_params_space(data_set):
+    return {
+        'metric': ['cosine', 'euclidean', 'manhattan', 'braycurtis',
+                   'canberra', 'chebyshev', 'seuclidean', 'sqeuclidean']
+    }
