efca55b463fbf7c1dc9482389ada35e75857e375
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2018-11-23 19:52:34 +0100

    Improve benchmark script.
    Make it possible to run any combination of classifier and datasets.

--- benchmark.py
+++ benchmark.py
@@ -18,6 +18,32 @@ ALGORITHMS = [path.splitext(f)[0]
 DATA_SETS = ['orl', 'mnist']
 
 
+def import_data_sets(data_set):
+    if data_set == 'all':
+        return [importlib.import_module(ds) for ds in DATA_SETS]
+    if data_set not in DATA_SETS:
+        msg = 'Unknown data set "%s"!' % data_set
+        raise ap.ArgumentTypeError(msg)
+    return [importlib.import_module(data_set)]
+
+
+def import_algorithms(algorithm):
+    if algorithm == 'all':
+        return [importlib.import_module(f'algorithms.{al}') for al in ALGORITHMS]
+    if algorithm in ALGORITHMS:
+        return [importlib.import_module(f'algorithms.{algorithm}')]
+
+    algorithms = []
+    names = algorithm.split(',')
+    for name in algorithm.split(','):
+        if name not in ALGORITHMS:
+            msg = 'Unknown algorithm "%s"!' % name
+            raise ap.ArgumentTypeError(msg)
+        module = importlib.import_module(f'algorithms.{name}')
+        algorithms.append(module)
+    return algorithms
+
+
 def get_result_path(subdir, classifier_name, data_set_name, with_pca):
     pca_suffix = 'with_pca' if with_pca else 'without_pca'
     file_name = '%s_%s_%s.json' % (classifier_name, data_set_name, pca_suffix)
@@ -38,20 +64,33 @@ def fetch_best_params(classifier_name, data_set_name, with_pca):
         return json.load(file)['best_params']
 
 
-def benchmark(algo, X, y, data_set_name, with_pca, n_folds, random_state, n_iterations, n_jobs=-1):
+def run_benchmark(algo, data_set, pca_components, n_folds, random_state, n_repeats, n_jobs):
     start_time = time.time()
 
     classifier_name = algo.__name__.replace('algorithms.', '')
+    data_set_name = data_set.__name__
 
-    print('Benchmarking %s' % classifier_name)
+    print(f'Benchmarking {classifier_name} on {data_set_name} (PCA={pca_components})')
 
-    best_params = fetch_best_params(classifier_name, data_set_name, with_pca)
+    print('Loading %s...' % data_set_name)
+    X_train, X_test, y_train, y_test = data_set.load_data()
+    X = np.concatenate((X_train, X_test))
+    y = np.concatenate((y_train, y_test))
+
+    use_pca = pca_components is not None
+
+    if pca_components is not None:
+        print('Applying PCA...')
+        pca = PCA(n_components=pca_components)
+        X = pca.fit_transform(X, y)
+
+    best_params = fetch_best_params(classifier_name, data_set_name, use_pca)
     print(best_params)
 
     kfold = RepeatedStratifiedKFold(
         n_splits=n_folds,
         random_state=random_state,
-        n_repeats=n_iterations
+        n_repeats=n_repeats
     )
     classifier = algo.get_classifier()
     classifier.set_params(**best_params)
@@ -65,10 +104,10 @@ def benchmark(algo, X, y, data_set_name, with_pca, n_folds, random_state, n_iter
     print(final_scores)
 
     exec_time_sec = (time.time() - start_time)
-    return {
+    benchmark_results = {
         'algorithm': classifier_name,
         'data_set': data_set_name,
-        'pca': with_pca,
+        'pca': use_pca,
         'params': best_params,
         'execution_time_sec': exec_time_sec,
         'n_jobs': n_jobs,
@@ -81,38 +120,10 @@ def benchmark(algo, X, y, data_set_name, with_pca, n_folds, random_state, n_iter
         },
         'scores': list(final_scores)
     }
-
-
-def run(args, data_set, with_pca):
-    data_set_name = data_set.__name__
-
-    print('Loading %s...' % data_set_name)
-    X_train, X_test, y_train, y_test = data_set.load_data()
-    X = np.concatenate((X_train, X_test))
-    y = np.concatenate((y_train, y_test))
-
-    if with_pca:
-        print('Applying PCA...')
-        pca = PCA(n_components=2)
-        X = pca.fit_transform(X, y)
-
-    n_folds = args.folds
-    random_state = args.random_state
-    n_repeats = args.repeats
-
-    for module_name in ALGORITHMS:
-        pca_suffix = 'pca' if with_pca else 'original'
-        file_name = '%s_%s_%s.json' % (data_set_name, pca_suffix, module_name)
-        file_path = os.path.join('benchmark_results', file_name)
-
-        if os.path.exists(file_path):
-            print('Benchmark data already exists for {}!'.format(module_name))
-            continue
-
-        algo = importlib.import_module('algorithms.%s' % module_name)
-        results = benchmark(algo, X, y, data_set_name, with_pca,
-                            n_folds, random_state, n_repeats)
-        write_json(results, file_path)
+    pca_suffix = f'pca{pca_components}' if use_pca else 'original'
+    file_name = '%s_%s_%s.json' % (data_set_name, pca_suffix, classifier_name)
+    file_path = os.path.join('benchmark_results', file_name)
+    write_json(benchmark_results, file_path)
 
 
 def parse_args():
@@ -133,16 +144,47 @@ def parse_args():
                         type=int,
                         default=33,
                         help='Number of times to repeat the cross-validation.')
+    parser.add_argument('--threads', '-t',
+                        required=False,
+                        type=int,
+                        default=-1,
+                        help='Number of threads to utilise. For all available threads use -1.')
+    parser.add_argument('--data-sets', '-d',
+                        required=False,
+                        type=import_data_sets,
+                        default='all',
+                        help=f'Supported values: all, {", ".join(DATA_SETS)}.')
+    parser.add_argument('--algorithms', '-a',
+                        required=False,
+                        type=import_algorithms,
+                        default='all',
+                        help=f'Supported values: all, {", ".join(ALGORITHMS)}.')
+    parser.add_argument('--pca', '-p',
+                        required=False,
+                        type=str,
+                        default='all',
+                        help=f'Supported values: all, original, 2d.')
     return parser.parse_args()
 
 
 def main():
-    print('Benchmarking')
     args = parse_args()
-    data_sets = [importlib.import_module(ds) for ds in DATA_SETS]
-    for ds in data_sets:
-        run(args, data_set=ds, with_pca=True)
-        run(args, data_set=ds, with_pca=False)
+
+    n_folds = args.folds
+    random_state = args.random_state
+    n_repeats = args.repeats
+    n_jobs = args.threads
+
+    print('Benchmark starting...')
+
+    for ds in args.data_sets:
+        for algorithm in args.algorithms:
+            if args.pca in ['all', '2d']:
+                run_benchmark(algo=algorithm, data_set=ds, pca_components=2, n_folds=n_folds,
+                              random_state=random_state, n_repeats=n_repeats, n_jobs=n_jobs)
+            if args.pca in ['all', 'original']:
+                run_benchmark(algo=algorithm, data_set=ds, pca_components=None, n_folds=n_folds,
+                              random_state=random_state, n_repeats=n_repeats, n_jobs=n_jobs)
     print('Benchmark done!')
 
 
