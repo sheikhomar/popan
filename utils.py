5d7981878cb931d3839c5b5838f372c5ca20d421
Omar Ali Sheikh <sheikhomar@users.noreply.github.com>
2018-11-24 13:57:48 +0100

    Handy methods.

--- utils.py
+++ utils.py
@@ -1,5 +1,30 @@
+import json
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+import importlib
+
+
+def get_best_params(classifier_name, data_set_name, with_pca):
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    params_dir = os.path.join(base_dir, 'params')
+    pca_suffix = 'with_pca' if with_pca else 'without_pca'
+    file_name = '%s_%s_%s.json' % (classifier_name, data_set_name, pca_suffix)
+    file_path = os.path.join(params_dir, file_name)
+    if not os.path.exists(file_path):
+        err_msg = f'Path "{file_path}" does not exist. Run ./hypertune.py first.'
+        raise ValueError(err_msg)
+    with open(file_path, 'r') as file:
+        return json.load(file)['best_params']
+
+
+def load_classifier_with_best_params(classifier_name, data_set_name, with_pca):
+    module = importlib.import_module(f'algorithms.{classifier_name}')
+    classifier = module.get_classifier()
+    best_params = get_best_params(classifier_name, data_set_name, with_pca)
+    classifier.set_params(**best_params)
+    return classifier
+
 
 def plot_images(images, image_shape=(30, 40), image_transposed=True, n_rows=5, n_columns=10, fig_width=8.0):
     heights = image_shape[1]* np.ones(n_rows)
